ef24340abb7dd8dca6704288b173204cf03d5f91
abishekvp <abirajvp@gmail.com>
2025-03-27 17:53:59 +0530

    Endless Update

--- internal/provider/function.go
+++ internal/provider/function.go
@@ -59,7 +59,7 @@ func isValidURL(input string) bool {
 		return false
 	}
 
-	hostnameRegex := `^(localhost|([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})$`
+	hostnameRegex := `^(localhost|([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}|(\d{1,3}\.){3}\d{1,3})$`
 	matched, _ := regexp.MatchString(hostnameRegex, host)
 	return matched
 }
